3c0a22270773134cb82b4cfa6bf8abe28c1ef1c4
terma <artem.stasuk@gmail.com>
2016-03-17 23:44:35 -0400

    More tests

---
+++ src/test/java/com/github/terma/fastselect/data/MultiByteDataTest.java
@@ -0,0 +1,74 @@
+/*
+Copyright 2015-2016 Artem Stasiuk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+package com.github.terma.fastselect.data;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultiByteDataTest {
+
+    @Test
+    public void provideAllocatedSize() {
+        MultiByteData data = new MultiByteData(100);
+        Assert.assertEquals(Data.DEFAULT_SIZE, data.allocatedSize());
+
+        for (byte i = 0; i < 50; i++) data.add(new byte[]{i});
+        Assert.assertEquals(Data.DEFAULT_SIZE + 100, data.allocatedSize());
+    }
+
+    @Test
+    public void supportCompact() {
+        MultiByteData data = new MultiByteData(100);
+        for (byte i = 0; i < 17; i++) data.add(new byte[]{i});
+        Assert.assertEquals(116, data.allocatedSize());
+
+        data.compact();
+
+        Assert.assertEquals(17, data.allocatedSize());
+        for (byte i = 0; i < data.size(); i++) Assert.assertArrayEquals(new byte[]{i}, (byte[]) data.get(i));
+    }
+
+    @Test
+    public void provideMemSize() {
+        MultiByteData data = new MultiByteData(100);
+        Assert.assertEquals(168, data.mem());
+
+        for (byte i = 0; i < 50; i++) data.add(new byte[] {i});
+        Assert.assertEquals(668, data.mem());
+    }
+
+    @Test
+    public void provideInc() {
+        Assert.assertEquals(33, new MultiByteData(33).inc());
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void dontSupportCompare() {
+        new MultiByteData(22).compare(0, 1);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void dontSupportPlainCheckAsOldApproach() {
+        new MultiByteData(22).plainCheck(0, null);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void dontSupportCheckAsOldApproach() {
+        new MultiByteData(22).check(0, null);
+    }
+
+}
